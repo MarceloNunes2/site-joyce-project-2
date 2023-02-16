fec3ece4f57442fa703dd21c956c0fda4195d2e0
marcelonunes2 <mklelo1990@gmail.com>
2023-02-16 11:45:28 -0300

    alterações

--- script.js
+++ script.js
@@ -7,15 +7,23 @@ let currentSlide = 0;
 function hideCurrentSlide() {
     slides[currentSlide].style.marginLeft = '-100vw';
     slides[currentSlide].style.visibility = 'hidden';
+    slip[currentSlide].style.marginLeft = '-100vw';
+    slip[currentSlide].style.visibility = 'hidden';
     pointers[currentSlide].classList.remove('active');
+    pointer[currentSlide].classList.remove('active');
+    
 }
 
 function showNextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
+    currentSlide = (currentSlide + 1) % slides.length, slip.length;
     slides[currentSlide].style.marginLeft = '0vw';
     slides[currentSlide].style.visibility = 'visible';
     pointers[currentSlide].classList.add('active');
+    slip[currentSlide].style.marginLeft = '0vw';
+    slip[currentSlide].style.visibility = 'visible';
+    pointer[currentSlide].classList.add('active');
 }
+
 
 function nextSlide() {
     hideCurrentSlide();
@@ -31,8 +39,20 @@ pointers.forEach((pointer, index) => {
         currentSlide = index;
         showNextSlide();
         slideInterval = setInterval(nextSlide, 5000);
+        
     });
 });
+pointer.forEach((pointers, index) => {
+    pointers.addEventListener('click', () => {
+        clearInterval(slideInterval);
+        hideCurrentSlide();
+        currentSlide = index;
+        showNextSlide();
+        slideInterval = setInterval(nextSlide, 5000);
+        
+    });
+});
+
 
 
 
